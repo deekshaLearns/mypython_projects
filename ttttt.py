26e8bdc6ac834beb8ed69daea8d2dfb622a8bc15
deekshaLearns <deeksha.codes@gmail.com>
2024-12-14 16:19:26 +0530

    Add files via upload

---
+++ ttttt.py
@@ -0,0 +1,29 @@
+"""
+PROBLEM SOLVING- EASY LEVEL
+Q- Write a program to print number from 1 to 5 using a for loop
+expected output- 1 2 3 4 5
+
+"""
+
+#for i in range(1,6):
+    #print(i, end=" ")
+
+
+"""
+Q- Write a program to print the squares of numbers from 1 to 5
+expected output - 1  4 9 16 25
+
+"""
+
+#for i in range(1, 6):
+    #print(i ** 2, end= " ")
+        
+
+"""
+Q- Write a program to print all even numbers from 1 to 10
+expected output- 2 4 6 8 10
+
+"""
+for i in range(1,11):
+    if i % 2 == 0:
+        print(i, end= " ")
